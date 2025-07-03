a5e72a34b73c4d22b92907e7e34e6680d1115a45
Rana <reduanulislam92665@gmail.com>
2025-07-04 05:56:03 +0600

    change of footer

--- src/components/shared/Footer.tsx
+++ src/components/shared/Footer.tsx
@@ -1,6 +1,6 @@
 export default function Footer() {
   return (
-    <footer className="bg-background  text-muted-foreground py-4 sticky bottom-0">
+    <footer className="bg-background/100  text-muted-foreground py-4 sticky bottom-0 backdrop-blur-xl z-50 transition-colors duration-300 ">
       <div className="container mx-auto text-center">
         <p>&copy; 2023 Book Management System. All rights reserved.</p>
       </div>
